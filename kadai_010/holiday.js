dc2a0f863b80f8ab96384e508452b9d4ab4f28a5
ukico534 <uki.kinoco@gmail.com>
2025-04-21 19:57:28 +0900

    Add files via upload

---
+++ kadai_010/holiday.js
@@ -0,0 +1,31 @@
+// 日本の祝日の配列
+const holidays = [
+  "元日",
+  "成人の日",
+  "建国記念の日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "スポーツの日",
+  "文化の日",
+  "勤労感謝の日",
+  "天皇誕生日"
+];
+
+console.log;
+for (let i = 0; i < holidays.length; i++) {
+  console.log(holidays[i]);
+}
+
+console.log;
+let j = 0;
+while (j < holidays.length) {
+  console.log(holidays[j]);
+  j++;
+}
